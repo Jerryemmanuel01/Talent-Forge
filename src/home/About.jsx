13f6ef774968cefea89d85d516c4fcf44d8038f0
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-02-22 00:35:55 +0100

    about section completed

--- src/home/About.jsx
+++ src/home/About.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import AboutImg from "../assets/talent-forge02.jpg";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about-sec">
+      <div className="container">
+        <div className="head--sec d-flex justify-content-center shadow-sm">
+          <h1 className="fw-bold head-txt fs-1">ABOUT</h1>
+        </div>
 
-export default About
+        {/* About body */}
+
+        <div className="row justify-content-center mt-5 gap-5">
+          {/* about image */}
+          <div className="col-lg-5 col-12">
+            <img src={AboutImg} alt="" className="about--img card" />
+          </div>
+
+          {/* about text */}
+          <div className="col-lg-6 col-12 fs-4">
+            We optimize organizational success by seamlessly aligning diverse
+            talents with opportunities, utilizing a collaborative and
+            client-centric approach, transforming potential into unparalleled
+            performance and shaping a future of continuous growth and excellence
+            for businesses
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
